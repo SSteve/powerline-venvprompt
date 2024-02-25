b2c3aba355ece52ed171c2f9254099a342cc65dd
Steve Nicholson <ssteve@mac.com>
2024-02-25 11:29:42 -0800

    First working version.

--- powerline_venvprompt/segments.py
+++ powerline_venvprompt/segments.py
@@ -1,7 +1,6 @@
 # vim:fileencoding=utf-8:noet
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-import configparser
 import os
 
 from powerline.theme import requires_segment_info
@@ -21,27 +20,42 @@ def prompt(pl, segment_info, ignore_venv=False, ignore_conda=False, ignored_name
 	:param list ignored_names:
 		Names of venvs to ignore. Will then get the name of the venv by ascending to the parent directory.
     """
-    pl.error("In powerline-venvprompt")
-    return "blah"
-    if not ignore_venv:
-        for candidate in reversed(segment_info['environ'].get('VIRTUAL_ENV', '').split("/")):
-            if candidate and candidate not in ignored_names:
-                return candidate
-    if not ignore_conda:
-        for candidate in reversed(segment_info['environ'].get('CONDA_DEFAULT_ENV', '').split("/")):
-            if candidate and candidate not in ignored_names:
-                return candidate
-    return None
-
     venv_path = segment_info['environ'].get('VIRTUAL_ENV', '')
 
     if venv_path is None:
         return None
     
-    config_file_name = os.join(venv_path, "pyvenv.cfg")
-    if (os.path.exists(config_file_name) is False):
-        return os.path.basename(venv_path)
-    
-    config = configparser.ConfigParser()
-    config.read(config_file_name)
-    return config['prompt'] or os.path.basename(venv_path)
+    config_file_name = os.path.join(venv_path, "pyvenv.cfg")
+    if os.path.exists(config_file_name):
+        prompt = None
+        with open(config_file_name, "r") as config:
+            for line in config.readlines():
+                key, value = line.split('=')
+                if key.strip() == 'prompt':
+                    # Strip whitespace, single quotes, and double quotes.
+                    prompt = value.strip().strip("'").strip('"')
+                    break
+            
+        if prompt:
+            return [{
+                'contents': prompt,
+                'highlight_groups': ["virtualenv"],
+            }]
+
+    if not ignore_venv:
+        for candidate in reversed(segment_info['environ'].get('VIRTUAL_ENV', '').split("/")):
+            if candidate and candidate not in ignored_names:
+                return [{
+                    'contents': candidate,
+                    'highlight_groups': ["virtualenv"],
+                }]
+
+    if not ignore_conda:
+        for candidate in reversed(segment_info['environ'].get('CONDA_DEFAULT_ENV', '').split("/")):
+            if candidate and candidate not in ignored_names:
+                return [{
+                    'contents': candidate,
+                    'highlight_groups': ["virtualenv"],
+                }]
+    return None
+
